da422576be7a47373cd0835be42a3724924c5aa9
Alpharius <52582478+z4Zachariah@users.noreply.github.com>
2021-10-05 07:46:59 +1300

    completed module 6

---
+++ storage/cms/cache/a7/db/404.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms615a0f8ccd57d792104323_a0140f9efab1a84824beb8928dc61e99Class extends Cms\Classes\PageCode
+{
+
+}
---
+++ storage/cms/twig/c0/c001c5c974e1c4b446099f3754f8caac6e7f0b3809185478c7a9e86f0d8acada.php
@@ -0,0 +1,94 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* C:\wamp64\www\bit703\module6\October/themes/demo/pages/404.htm */
+class __TwigTemplate_1682c2fe2ab9d7afa79690381bd877e1be091eee6875de092e5c108ec5a9346e extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"jumbotron\">
+    <div class=\"container\">
+        <h1>Page not found</h1>
+        <p>We're sorry, but the page you requested cannot be found.</p>
+    </div>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "C:\\wamp64\\www\\bit703\\module6\\October/themes/demo/pages/404.htm";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<div class=\"jumbotron\">
+    <div class=\"container\">
+        <h1>Page not found</h1>
+        <p>We're sorry, but the page you requested cannot be found.</p>
+    </div>
+</div>", "C:\\wamp64\\www\\bit703\\module6\\October/themes/demo/pages/404.htm", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
